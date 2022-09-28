bf901a4cc44ffc10d348c2664ab3c160c469b3e2
Sukhdev Hansda <sukhdevhansda8@gmail.com>
2022-09-28 23:24:46 +0530

    Add dashboard route in allroutes page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,10 @@
 import './App.css';
-import  Navbar  from './Components/Navbar';
-import Home from './Components/Home';
-
-import Dashboard from './Components/Dashboard';
-
-import SignIn from './Components/SignIn';
-import SignUp from './Components/SignUp';
 import Allroutes from './Components/Allroutes';
 
 
 function App() {
   return (
     <div className="App">
-       {/* <Navbar/> */}
      <Allroutes/>
     </div>
   );
