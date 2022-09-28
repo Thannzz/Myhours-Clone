d0f1a9b8cfd1a1672dfc63ac4268df9a288764cb
Sukhdev Hansda <sukhdevhansda8@gmail.com>
2022-09-29 01:45:25 +0530

    Create Track component

--- frontend/src/Components/Content.jsx
+++ frontend/src/Components/Content.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import "../styles/Dashboard.css"
+import Track from './Track'
+// import Project1 from "./ProjectsPages/Project1.jsx"
 
 export default function Content() {
   return (
     <div className='content'>
+    {/* <Project1/> */}
+    <Track/>
       
     </div>
   )
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -7,15 +7,18 @@ import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "./store/index";
 import { ChakraProvider } from "@chakra-ui/react";
+import AppContextProvider from "./context/Appcontext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-    <ChakraProvider>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <ChakraProvider>
+        <AppContextProvider>
+          <BrowserRouter>
+            <App />
+          </BrowserRouter>
+        </AppContextProvider>
       </ChakraProvider>
     </Provider>
   </React.StrictMode>
