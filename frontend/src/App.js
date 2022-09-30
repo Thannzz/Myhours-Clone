f5d86034bd157ba7061a343f2eb464e2cbf2e0a0
Abrar2003 <spartanabrar@gmail.com>
2022-09-30 10:18:27 +0530

    initial commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,13 @@
-import "./App.css";
-import Allroutes from "./Components/Allroutes";
+import './App.css';
+import Allroutes from './Components/Allroutes';
+import Projects from './Components/ProjectsPages/Project1';
+import ProjectForm from './Components/ProjectsPages/ProjectForm';
+
+
 function App() {
   return (
     <div className="App">
-      <Allroutes />
+     <Allroutes />
     </div>
   );
 }
